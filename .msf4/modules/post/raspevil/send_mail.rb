197e8559f91845fa5d3528e8db857637110a83e9
Xavier Le Cunff <lecunffxavier@gmail.com>
2020-01-12 03:20:55 +0100

    e-mail configuration finished

---
+++ .msf4/modules/post/raspevil/send_mail.rb
@@ -0,0 +1,26 @@
+##
+# This module requires Metasploit: https://metasploit.com/download
+# Current source: https://github.com/rapid7/metasploit-framework
+##
+
+class MetasploitModule < Msf::Post
+
+  def initialize(info = {})
+    super(update_info(info,
+        'Name'          => '[Platform] [Module Category] [Software] [Function]',
+        'Description'   => %q{
+          Say something that the user might want to know.
+        },
+        'License'       => MSF_LICENSE,
+        'Author'        => [ 'Name' ],
+        'Platform'      => [ 'android', 'win', 'linux', 'osx', 'unix', 'bsd', 'solaris' ],
+        'SessionTypes'  => [ 'meterpreter', 'shell' ]
+    ))
+  end
+
+  def run
+    puts 'Hello, world!'
+    exec '~/test.sh'
+  end
+
+end
--- install.sh
+++ install.sh
@@ -26,6 +26,17 @@ isInstalled() {
     fi
 }
 
+isInstalledSnap() {
+    installed=$(command -v $1)
+    if [ "$installed" == "" ]
+    then
+        echo -e "\e[31m[ - ] $1 not yet installed\e[0m"
+        sudo snap install $1
+    else
+        echo -e "\e[32m[ + ] $1 already installed\e[0m"
+    fi
+}
+
 checkRoot() {
     if [ "$EUID" -ne 0 ]
     then echo -e "\e[31mPlease run as root\e[m"
@@ -39,9 +50,9 @@ isInstalled metasploit-framework
 isInstalled ca-certificates
 isInstalled msmtp
 isInstalled screen
-mkdir -p ~/raspevil
-mkdir -p ~/raspevil/backdoors
-mkdir -p ~/raspevil/credentials
-mkdir -p ~/raspevil/tmp
-touch ~/raspevil/emails
+isInstalled snapd
+isInstalledSnap ngrok
 touch ~/.msmtprc
+cp -r raspevil-config ~/raspevil
+cp -r .msf4 ~
+sudo cp res/* /usr/local/bin
